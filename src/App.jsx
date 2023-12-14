31ffa84bf6a13ca0132e5bba2a850eee71e6464d
sharadvamsi <sharadvamsi19@gmail.com>
2023-12-14 17:39:38 +0530

    created pathes add added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-const App = ()=> {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Sigin";
+import Signup from "./pages/SignUP";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+const App = () => {
   return (
-    <>
-     <h1 >
-      Hello world!
-    </h1>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
