c12bc4b8f980b80fbcf90ebf5ce219d92ed1562b
sharadvamsi <sharadvamsi19@gmail.com>
2023-12-15 20:49:35 +0530

    completed signup page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signin from "./pages/Sigin";
-import Signup from "./pages/SignUP";
+import Signup from "./pages/Signup";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from './components/Header'
